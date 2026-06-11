e71b5724041a72f8b84ba3e6e26fb9de8580d0d6
vekariyaparam14-cyber <vekariyaparam14@gmail.com>
2026-06-11 15:17:44 +0530

    commit

--- PYTHON ASSIGNMENT/Session-1/hello_app.py
+++ PYTHON ASSIGNMENT/Session-1/hello_app.py
@@ -1,1 +1,5 @@
+# 4.Create a Python script file called hello_app.py (not a notebook) that 
+# prints 'Hello from .py file!' when run from the terminal.<br><br><em><strong>
+# Hint:</strong> Use a text editor like VS Code or Notepad to create the file, then run it using the command: python hello_app.py</em>
+
 print("Hello World!")
